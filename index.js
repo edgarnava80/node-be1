9d80564aeb207e03b96f808430b5a0487b2fef5d
Edgar Nava <edgarnava2680@gmail.com>
2023-03-28 04:57:58 -0500

    Test 1

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hola desde app!'));
+app.get('/', (req, res) => res.send('Hola desde app updated with github actions!!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
