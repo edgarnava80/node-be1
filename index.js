c59daf25a3232d13d8f0c14aac7c7f293158b42a
Edgar Nava <edgarnava2680@gmail.com>
2023-03-28 03:02:23 -0500

    Starter files

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hola desde app!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
