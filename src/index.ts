47ae210a48ab4c2f6b008fe8f4349d58dcafccce
Edgar Nava <edgarnava2680@gmail.com>
2023-03-30 02:49:06 -0500

    Added h1

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import express, { Request, Response} from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => res.send('Hola desde app updated with github actionssss!!'));
+app.get('/', (req: Request, res: Response) => res.send('<h1>Now with TypeScript!</h1>'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
