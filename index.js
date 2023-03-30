95c073d62b6722249eeadea72f766c7b7be823c3
Edgar Nava <edgarnava2680@gmail.com>
2023-03-30 02:44:29 -0500

    Incorporated typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Hola desde app updated with github actionssss!!'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
